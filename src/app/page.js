737cce392bc15cb5eeb42eca3bc50df21729257a
sibylaaal <bybill757@gmail.com>
2024-02-17 22:50:10 +0100

    Jj

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,6 @@ import Hero from "./components/hero";
 import Featured from "./components/feautred";
 import Section from "./components/section";
 import Services from "./components/services";
-import Contact from "./components/contact";
 import Footer from "./components/footer";
 
 export default function Home() {
